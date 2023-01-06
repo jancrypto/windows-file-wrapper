ebe0b746988263b0e38dde56d8c8f08ff4ec4a7a
Jan <96467770+jancrypto@users.noreply.github.com>
2023-01-06 13:59:58 +0100

    Update filemanagment.cpp

--- filemanagment.cpp
+++ filemanagment.cpp
@@ -123,3 +123,11 @@ bool filemanagment::exists(File f) {
 	CloseHandle(h);
 	return result;
 }
+
+std::string filemanagment::get_working_directory()
+{
+	char buffer[MAX_PATH] = { 0 };
+	GetModuleFileNameA(NULL, buffer, MAX_PATH);
+	std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+	return std::string(buffer).substr(0, pos);
+}
